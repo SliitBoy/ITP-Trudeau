c440d41b6cb0b0594cdb817f476b5c8c4481a285
SliitBoy <63994867+SliitBoy@users.noreply.github.com>
2020-08-23 13:12:46 +0530

    Sidebar styling

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -8,16 +8,16 @@
       right
       shadow
       visible="true"
+      bg-variant="dark"
+      text-variant="light"
     >
       <div class="px-3 py-2 vh-100 w-100">
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-        </p>
-        <b-img
-          src="https://picsum.photos/500/500/?image=54"
-          fluid
-          thumbnail
-        ></b-img>
+        <div style="height: 200px">
+          Notifications Here
+        </div>
+        <div>
+          Calendar Here
+        </div>
       </div>
     </b-sidebar>
   </div>
