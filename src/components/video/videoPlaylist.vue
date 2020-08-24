ff3351be76d0e93c89668be63abf536e6ac88f8d
SliitBoy <63994867+SliitBoy@users.noreply.github.com>
2020-08-24 17:19:15 +0530

    Added create new playlist dropdown form

--- src/components/video/videoPlaylist.vue
+++ src/components/video/videoPlaylist.vue
@@ -1,3 +1,69 @@
 <template>
-  <h1>Video Playlist</h1>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col 8"></div>
+      <div class="col-2">
+        <b-dropdown
+          id="dropdown-form"
+          text="New Playlist"
+          ref="dropdown"
+          class="m-2"
+          variant="dark"
+        >
+          <b-dropdown-form>
+            <b-form-group
+              label="Name"
+              label-for="form-new-playlist-name"
+              @submit.stop.prevent
+            >
+              <b-form-input
+                id="form-new-playlist-name"
+                size="sm"
+                v-model="playListName"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+              label="Description"
+              label-for="form-new-playlist-desc"
+            >
+              <b-form-input
+                id="form-new-playlist-desc"
+                type="text"
+                size="sm"
+                v-model="playListDescription"
+              ></b-form-input>
+            </b-form-group>
+            <b-button variant="primary" size="sm" @click="createNewPlaylist"
+              >Add</b-button
+            >
+          </b-dropdown-form>
+        </b-dropdown>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      playListName: "",
+      playListDescription: ""
+    };
+  },
+
+  methods: {
+    createNewPlaylist() {
+      const playListFormData = {
+        playListName: this.playListName,
+        playListDescription: this.playListDescription
+      };
+      axios.post("https://trudeau-video.firebaseio.com/playlists.json",playListFormData)
+        .then(resp => console.log(resp))
+        .catch(error => console.error(error));
+    }
+  }
+};
+</script>
