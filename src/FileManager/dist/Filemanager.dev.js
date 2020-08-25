4ea6ce2aee7c7a1259a9281e492f9b2ea2db7b07
bishan-kulasekara <56211535+bishan-kulasekara@users.noreply.github.com>
2020-08-25 22:48:31 +0530

    initial commit file manager.js
    basic functionality for file manager

---
+++ src/FileManager/dist/Filemanager.dev.js
@@ -0,0 +1,66 @@
+"use strict";
+
+var fs = require('fs');
+
+var p = require('path');
+
+var fsExtra = require('fs-extra'); //const rimraf = require("rimraf");
+//const test = require("test.js");
+
+
+var rootDIR = p.join(__dirname, '/root');
+createDirectory(rootDIR, "test");
+createRootDirectory(rootDIR); //deleteDirectory(rootDIR);
+
+console.log(fileCreatedDate(rootDIR)); //emptyDirectory(rootDIR);
+
+function createDirectory(path, name) {
+  var dir = p.join(path, "/" + name); //set dir to database
+
+  console.log('creating file ');
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+    console.log('Directory ' + name + ' created');
+  } else {
+    console.log('Directory with name "' + name + '" exists in path"' + path + '"');
+  }
+}
+
+function createRootDirectory() {
+  var dir = p.join(__dirname, '/root');
+  console.log("creating file " + dir); //set to save in database
+
+  var date_ob = new Date(); //get current time to work
+
+  var columns = ['root', rootDIR, 1, '', '']; //insert('directory', columnnames, values);
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+    console.log('root directory created');
+  } else {
+    console.log('root directory exists');
+  }
+}
+
+function emptyDirectory(path) {
+  fsExtra.emptyDirSync(path);
+}
+
+function rename(file, rename) {
+  fs.rename();
+}
+
+function createfile() {
+  fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+}
+
+function fileCreatedDate(file) {
+  var _fs$statSync = fs.statSync(file),
+      birthtime = _fs$statSync.birthtime;
+
+  return birthtime;
+}
