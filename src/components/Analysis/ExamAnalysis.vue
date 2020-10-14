7c10c958e6dc740fbd913579dfc7dfde8a57c82b
Tharu98-n <65774429+Tharu98-n@users.noreply.github.com>
2020-10-15 01:26:16 +0530

    fixed css

--- src/components/Analysis/ExamAnalysis.vue
+++ src/components/Analysis/ExamAnalysis.vue
@@ -1,115 +1,231 @@
-<template><div class="bg">
-    <label>  {{CourseName}} - {{SubCode}}  </label>
-    <div class="bar">
-    <b-input-group size="sm" class="mb-2">
-      <b-input-group-prepend is-text>
-        <b-icon icon="search"></b-icon>
-      </b-input-group-prepend>
-      <b-form-input type="search" width="200%%" placeholder="Search Course"></b-form-input>
-    </b-input-group>
-    </div><br><br>
-    <label> Exam Analysis </label><br>
+<template>
+<div style="background-color: #2C2F33;" class="height">
+    <h1 style="color: aliceblue;font-family:sans-serif">{{this.$route.params.name}}</h1>
+
     <br>
-    <table table border="1"  >
+    <br>
+
+    <h5 style="color: aliceblue;font-family:sans-serif">Student Marks Analysis</h5>
+
+    <br>
+    <br>
+
+    <table class="tab">
         <tr>
-            <th> Quiz</th>
-            <th> Total Marks </th>
-            <th> Status </th>
+            <td class="tdBtn">
+                <b-button variant="outline-success" class="btnGrp" size="sm" type = "submit" @click="ViewResults()">View Results</b-button>
+            </td>
+            <td class="tdBtn">
+            </td>
+            <td class="tdBtn">
+            </td>
+            <td class="tdBtn">
+
+                <b-button variant="outline-success" class="btnGrp" size="sm"  v-b-modal.modal-prevent-closing >Add New Student</b-button>
+
+            </td>
         </tr>
-        <tr>
-            <td><br></td>
-            <td><br></td>
-            <td><br></td>
+    </table>
+    <br>
+    <table class="tabSt" border="3">
+        <tr style="font-weight:bold;color: aliceblue;font-family:sans-serif;font-size:20px">
+            <td>
+                Student Name
+            </td>
+            <td>
+                Grade
+            </td>
+            <td>
+                Status
+            </td>
 
         </tr>
-        <tr>
-            <td><br></td>
-            <td><br></td>
-            <td><br></td>
-    
+        <tr v-for="c in courseFilter" v-bind:key="c.id" style="color: aliceblue;font-family:sans-serif">
+            <td>
+                {{c.StName}}
+            </td>
+            <td>
+                {{c.grade}}
+            </td>
+            <td>
+                {{c.status}}
+            </td>
         </tr>
-        <tr>
-            <td><br></td>
-            <td><br></td>
-            <td><br></td>
-    
-        </tr>
-        <tr>
-            <td><br></td>
-            <td><br></td>
-            <td><br></td>
-    
-        </tr>
-                <tr>
-            <td><br></td>
-            <td><br></td>
-            <td><br></td>
-    
-        </tr>
-        <tr>
-            <td><br></td>
-            <td><br></td>
-            <td><br></td>
-    
-        </tr>
-        <tr>
-            <td><br></td>
-            <td><br></td>
-            <td><br></td>
-    
-        </tr>
-        <tr>
-            <td><br></td>
-            <td><br></td>
-            <td><br></td>
-    
-        </tr>
-        <tr>
-            <td><br></td>
-            <td><br></td>
-            <td><br></td>
-    
-        </tr>
-        <tr>
-            <td><br></td>
-            <td><br></td>
-            <td><br></td>
-    
-        </tr>
-        <br>
     </table>
-    </div>
+
+    <b-modal
+      id="modal-prevent-closing"
+      ref="modal"
+      title="Add New Student"
+      @show="resetModal"
+      @hidden="resetModal"
+      @ok="handleOk"
+    >
+    <form ref="form" @submit.stop.prevent="newStudent">
+       
+        <b-form-group
+          :state="StNameState"
+          label="Student Name"
+          label-for="stname-input"
+          invalid-feedback="Name is required"
+        >
+        <b-form-input
+            id="name-input"
+            v-model="StName"
+            style="color:black"
+            :state="StNameState"
+            required
+          ></b-form-input>
+        </b-form-group>
+        
+        <b-form-group
+          :state="gradeState"
+          label="Grade"
+          label-for="grade-input"
+          invalid-feedback="Grade is required"
+        >
+        <b-form-input
+            id="name-input"
+            v-model="grade"
+            :state="gradeState"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+         <b-form-group
+          :state="statusState"
+          label="Status "
+          label-for="stname-input"
+          invalid-feedback="Status is required"
+        >
+        <b-form-input
+            id="name-input"
+            v-model="status"
+            :state="statusState"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+    </form>
+     </b-modal>
+
+</div>
    
 </template>
 <script>
-import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-// Install BootstrapVue
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+import axios from 'axios'
+
 export default {
     data:function() {
         return{
-            CourseName:'Data Structures and Algorithms',
-            SubCode: 'IT2010'
+            name: this.$route.params.name,
+            StName:"",
+            StNameState:null,
+            grade:"",
+            gradeState:null,
+            status:"",
+            statusState:null,
+            studentArray:[],
+            result:[]
         }
+    },
+
+    computed: {
+    
+    courseFilter: function() {
+
+        return this.studentArray.filter(c=> {
+            return c.name.match(this.$route.params.name);
+        });
     }
+    },
+
+    methods: {
+
+
+      checkFormValidity() {
+        const valid = this.$refs.form.checkValidity()
+        this.StNameState = valid
+        this.gradeState = valid
+        this.statusState = valid
+        return valid
+      },
+
+
+      resetModal() {
+        this.StName = ''
+        this.StNameState = null
+        this.grade = ''
+        this.gradeState= null
+        this.status = ''
+        this.statusState = null
+      },
+
+
+      handleOk(bvModalEvt) {
+    
+        bvModalEvt.preventDefault()
+        
+        this.newStudent()
+      },
+
+      newStudent() {
+        
+        
+        if (!this.checkFormValidity()) {
+          return
+        }
+
+        const formData = {
+
+            StName:this.StName,
+            name:this.$route.params.name,
+            grade:this.grade,
+            status:this.status
+        };
+
+        console.log(formData)
+        axios.post('https://fir-2tharu.firebaseio.com/Analysis.json',formData)
+         .then(res=>console.log(res))
+         .catch(error=>console.log(error));
+        
+        // Hide the modal manually
+        this.$nextTick(() => {
+          this.$bvModal.hide('modal-prevent-closing')
+        })
+      },
+
+
+      ViewResults()
+            {
+            axios.get('https://fir-2tharu.firebaseio.com/Analysis.json')
+            .then(res => {
+                console.log(res);
+                const data = res.data;
+               
+                for(let key in data){
+
+                    const result = data[key];
+                    result.id = key;
+                    this.studentArray.push(result);
+    
+                }
+
+                console.log(this.studentArray);
+                //return this.student
+
+      })
+            .catch(error => console.log(error))
+        
+        },
+
+    
+}
 }
 </script>
 <style>
-label{
-    font-size:25px;
-    color:white;
-}
-table {
-    margin-left:auto;
-    margin-right:auto;
-    width:80%;
-    color:white;
-    border: white;
-}
+    
 .bg{
     background-color:#2C2F33;
     margin-right:0%;
@@ -121,13 +237,25 @@ table {
     position:relative;
 }
 
-.bar{
-  position: absolute;
-  right: 0px;
-  width: 400px;
+.tab{
+    width: 100%;
+    margin-top: 2%;
+    margin-bottom: 5%;
+
 }
 
+.tabSt{
 
+    width: 100%;
+}
 
+.tdBtn{
+    width: 25% 
+}
+
+.height{
+
+    height: 100vh;
+}
 
 </style>
