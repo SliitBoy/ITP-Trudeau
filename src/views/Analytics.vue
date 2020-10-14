614e0eafdda0102d23a2280087dc4cd34b6b4dec
Tharu98-n <65774429+Tharu98-n@users.noreply.github.com>
2020-10-15 01:43:53 +0530

    updated

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -1,18 +1,24 @@
-template>
-<div>
-  <router-link to="/createReport"  tag="ui-button" class="nav-item" activeClass="active"
-          ><a class="nav-link">Create Report</a></router-link>
-
-  <router-link to="/updateReport" tag="ui-bitton" class="nav-item" activeClass="active">
-           <a class="nav-link">UpdateReport</a></router-link>
-  <router-link to="/ExamAnalysis" tag="ui-button" class="nav-item" activeClass="active">
-        <a class="nav-link"> ExamAnalysis </a> </router-link>
-  <router-link to="/SemesterReportAdmin" tag="ui-button" class="nav-item" activeClass="active">
-        <a class="nav-link">SemesterReportAdmin</a></router-link> 
-<router-link to="/SemesterReport" tag="ui-button" class="nav-item" activeClass="active">
-        <a class="nav-link">SemesterReport</a></router-link>
+<template>
+<div style="background-color: #2C2F33;" class="height">
+  <router-link to="/ModuleList" tag="ui-button" class="nav-item" activeClass="active">
+        <a class="nav-link">Module List</a></router-link> 
+<router-link to="/ReportList" tag="ui-button" class="nav-item" activeClass="active">
+        <a class="nav-link">Report List</a></router-link>
+<router-link to="/StudentReport" tag="ui-button" class="nav-item" activeClass="active">
+        <a class="nav-link">Student Report </a></router-link>
 
 </div>
 </template>
+
+<style>
+
+.height{
+
+    height: 100vh;
+}
+
+</style>
+
+
 
 
