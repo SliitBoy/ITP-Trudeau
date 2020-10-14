8235fe277b69730cc7faeb94919a63c14ce96b0a
IT19070018 <it19070018@my.sliit.lk>
2020-10-15 01:18:59 +0530

    Fix home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="home"></div>
+  <div class="vh-100 overflow-hidden">
+    <b-jumbotron
+      bg-variant="dark"
+      text-variant="white"
+      class="vh-100 overflow-hidden"
+    >
+      <template v-slot:header>Trudeau</template>
+
+      <template v-slot:lead>
+        Welcome to Trudeau!
+      </template>
+
+      <hr class="my-4" />
+
+    </b-jumbotron>
+  </div>
 </template>
 
 <script>
