2cd4e87c864a1a43dc7d5032958695c4091d8faa
Tharu98-n <65774429+Tharu98-n@users.noreply.github.com>
2020-10-15 01:38:13 +0530

    css fixed

--- src/components/reports/UpdateReport.vue
+++ src/components/reports/UpdateReport.vue
@@ -1,35 +1,111 @@
 <template>
 
-    <div class="bg">
+    <div style="background-color: #2C2F33;" class="height">
+
     <h3>Update Report</h3>
     <br>
-    <label> ID Number : </label> 
-    <input type='text' placeholder="Enter your ID Number">
+      <label> ID Number : </label> 
+    <input type='text' :placeholder="this.$route.params.stId" v-model="stID">
     <br>
     <br>
     <label> Student Name  :  </label>
-    <input type ='text' placeholder="Enter Student's Name">
+    <input type ='text' :placeholder="this.$route.params.stName" v-model="stName">
     <br>
-    <br>
-    <div class="centre"><span style="padding-right: 18em">Course</span>Result
-    <br>
-    <br>
-    <input type="text" text style="margin-right:50px;" placeholder ="Course Name"><input type="text"  placeholder ="Result"><br><br>
-    <input type="text" text style="margin-right:50px;" placeholder ="Course Name"><input type="text"  placeholder ="Result"><br><br>
-    <input type="text" text style="margin-right:50px;" placeholder ="Course Name"><input type="text"  placeholder ="Result"><br><br>
-    <input type="text" text style="margin-right:50px;" placeholder ="Course Name"><input type="text"  placeholder ="Result"><br><br>
-    <input type="text" text style="margin-right:50px;" placeholder ="Course Name"><input type="text"  placeholder ="Result"><br><br>
- </div><br><div class="bottom"><div class="right">
-  <b-button style="margin-right:50px;" variant="success">Update</b-button>
-  <b-button style="margin-right:50px;" variant="danger">Delete</b-button>
+    <br><div class="centre">
+    <div><span style="padding-right: 18em">Course - Result</span></div>
+    <br> <div class="but">
 
-</div>
-</div>
+        <button @click="onAddCourse" type="button">Add Course</button>
+          <div class="course-list">
+            <div
+                    class="input"
+                    v-for="(courseInput, index) in courseInputs"
+                    v-bind:key="courseInput.id">
+              <label :for="courseInput.id">Course {{ index +1 }}</label>
+              <input
+                      type="text"
+                      :id="courseInput.id"
+                      v-model="courseInput.value">
+            </div>
+          </div>
+  </div></div>
+
+  
+  <b-button style="margin-left:80%;" variant="success" type="submit" @click="editReport">Update</b-button>
+  <b-button style="margin-left:4%;" variant="danger" type="submit" @click="deleteReport">Delete</b-button>
+
+
+
 </div>
 </template>
+
+
 <script>
+import axios from 'axios'
+
+
+    export default {
+
+        data(){
+
+            return{
+
+            id: this.$route.params.id,
+            stId: this.$route.params.stId,
+            stName: this.$route.params.stName,
+            courseInputs:[]
+
+            };
+
+        },
+
+        methods :{
+
+            
+        onAddCourse(){
+             const newCourse = {
+                 id : Math.random() * Math.random() * 1000,
+                 value:''
+             }
+
+             this.courseInputs.push(newCourse)
+        },
+
+            editReport(){
+
+                const formData = {
+
+                    stID:this.stID,
+                    stName:this.stName,
+                    courses:this.courseInputs.map(course=>course.value)
+
+                }
+
+                console.log(formData)
+                axios.patch('https://fir-2tharu.firebaseio.com/report/'+this.$route.params.id+'.json',formData)
+
+                .then(res=>console.log(res))
+
+                .catch(error=>console.log(error));
+            },
+        
+
+        deleteReport(){
+
+            axios
+            .delete('https://fir-2tharu.firebaseio.com/report/'+this.$route.params.id+'.json')
+
+            .then(res=>console.log(res))
+
+        }
+        }
+
+    }
+
 
 </script>
+
+
 
 <style>
 h3{
@@ -42,12 +118,6 @@ h3{
     width:50%;
    
 }
-.right{
-    position:absolute;
-    right:0px;
-    width:250px;
-    padding:5px;
-}           
 
 .bg{
     background-color:#2C2F33;
@@ -68,6 +138,17 @@ h3{
     height:100%;
     width:100%;
     position:relative;   
+      position:absolute;
+    right:0px;
+    width:250px;
+    padding:5px;
+} 
+
+.height{
+
+    height: 100vh;
 }
+
+
 
 </style>
